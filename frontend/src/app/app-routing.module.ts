ded28fbe37330bba6923c943411213f8ef8313d6
Kieu-Trinh522 <damkieutrinh522@gmail.com>
2021-01-26 14:42:16 +0700

    add all

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
 
 const routes: Routes = [
   {path:'employees',component:EmployeeListComponent},
+  {path:'add',component:CreateEmployeeComponent},
 ];
 
 @NgModule({
