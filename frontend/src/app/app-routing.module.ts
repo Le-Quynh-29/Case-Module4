5c910eb0938cd8e3dabf41d0c03e327f41978c80
Ta-Thi-Thuy <thuythita7202@gmail.com>
2021-01-26 17:36:08 +0700

    list-position

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PositionListComponent } from './position/position-list/position-list.component';
 
 const routes: Routes = [
-  {path:'employees',component:EmployeeListComponent},
+  {path:'employees',
+  component:EmployeeListComponent},
+  {path:'positions',
+  component:PositionListComponent}
+ 
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,16 +7,21 @@ import { HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { PositionListComponent } from './position/position-list/position-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    PositionListComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
